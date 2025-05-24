a4899cec78ceffb8d5e0177dce7be350708e4442
timdlyncme <timd@lyncme.ca>
2025-05-24 17:14:51 -0400

    Update badge.tsx

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -9,13 +9,13 @@ const badgeVariants = cva(
     variants: {
       variant: {
         default:
-          "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
+          "border-transparent bg-purple-950 text-white hover:bg-purple-700/80",
         secondary:
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         success:
-          "border-transparent bg-green-500 text-white hover:bg-green-500/80",
+          "border-transparent bg-green-950 text-white hover:bg-green-700/80",
         outline: "text-foreground",
       },
     },
