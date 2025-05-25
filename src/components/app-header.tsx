9ae73959c541508c717a2187e8a7d63fd1bd5c84
timdlyncme <timd@lyncme.ca>
2025-05-25 14:07:02 -0400

    Update app-header.tsx

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -49,7 +49,7 @@ export function AppHeader() {
             <DropdownMenuContent className="w-56" align="end" forceMount>
               <DropdownMenuLabel className="font-normal">
                 <div className="flex flex-col space-y-1">
-                  <p className="text-sm font-medium leading-none">{userName}</p>
+                  <p className="text-sm font-medium leading-none">{user.full_name}</p>
                   <p className="text-xs leading-none text-muted-foreground">
                     {user.email}
                   </p>
