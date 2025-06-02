db546a1e02c67e91a4a84b0d71334f28106cc738
timdlyncme <timd@lyncme.ca>
2025-06-02 14:39:45 -0400

    Update CloudAccounts.tsx

--- src/pages/CloudAccounts.tsx
+++ src/pages/CloudAccounts.tsx
@@ -298,7 +298,7 @@ const CloudAccounts = () => {
             if (!open) resetForm();
           }}>
             <DialogTrigger asChild>
-              <Button>
+              <Button style={{ backgroundColor: "#FF5100" }}>
                 <Plus className="h-4 w-4 mr-2" />
                 New Cloud Account
               </Button>
