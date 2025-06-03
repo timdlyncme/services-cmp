581fd88fe67afb26c1d5071349d054254a620961
timdlyncme <timd@lyncme.ca>
2025-06-03 19:05:34 -0400

    Update EnhancedDashboard.tsx

--- src/pages/EnhancedDashboard.tsx
+++ src/pages/EnhancedDashboard.tsx
@@ -402,7 +402,6 @@ export default function EnhancedDashboard() {
           </Button>
           
           <Button
-            style={{ backgroundColor: "#FF5100" }}
             onClick={() => setShowDashboardManager(true)}
           >
             <LayoutDashboard className="h-4 w-4 mr-2" />
