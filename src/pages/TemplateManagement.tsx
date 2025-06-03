4ed9071dd0f3e2268608b3901d94160a3b4c7950
timdlyncme <timd@lyncme.ca>
2025-06-03 19:07:38 -0400

    Update TemplateManagement.tsx

--- src/pages/TemplateManagement.tsx
+++ src/pages/TemplateManagement.tsx
@@ -218,7 +218,7 @@ const TemplateManagement = () => {
             <RefreshCw className="h-4 w-4" />
           </Button>
           {canManageTemplates && (
-            <Button style={{ backgroundColor: "#FF5100" }} onClick={() => setIsUploadWizardOpen(true)}>
+            <Button onClick={() => setIsUploadWizardOpen(true)}>
               <Plus className="h-4 w-4 mr-2" />
               Create Template
             </Button>
